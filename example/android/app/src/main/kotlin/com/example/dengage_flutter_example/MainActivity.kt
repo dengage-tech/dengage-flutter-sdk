4ecc6b86f7ce5ccfc21e16d4c09acfe94641114b
hasnain <hasnain.tanvir@dengage.com>
2022-08-14 19:51:37 +0500

    android geofence added

--- example/android/app/src/main/kotlin/com/example/dengage_flutter_example/MainActivity.kt
+++ example/android/app/src/main/kotlin/com/example/dengage_flutter_example/MainActivity.kt
@@ -5,14 +5,15 @@ import com.example.dengage_flutter.DengageCoordinator
 import io.flutter.embedding.android.FlutterActivity
 import io.flutter.embedding.android.FlutterFragmentActivity
 
-class MainActivity: FlutterFragmentActivity() {
+class MainActivity : FlutterFragmentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         DengageCoordinator.sharedInstance.setupDengage(
-                true,
-                "FEYl27JxJfay6TxiYCdlkP2FXeuhNfEoI8WkxI_p_l__s_l_5sLbzKmc9c88mSZxRCrLuqMK4y0e8nHajQnBt8poBNDMvNtIytYKZ6byBQZOE8kqkkgDnlye2Lb5AcW3tuIWQjYz",
-                "your-huawei-key-here",
-                applicationContext
+            true,
+            "7VJRT9FBZdxMBoe2pNX4_s_l_7z3SkzvQTMEoZ_s_l_MmV9QvULukRdACg_s_l_GyMduz_s_l_qlxyO4Bs9W4B6U007IgfiNCxvKL4fDz29kxT9qUUJHSzOLbaI24Co0N4Dw1sZrBuAWkKQCEV_p_l_0vXZtOSXV_s_l_jizKYpqGg_e_q__e_q_",
+            "your-huawei-key-here",
+            true,
+            applicationContext
         )
     }
 }
