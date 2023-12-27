9519c66f9ef5da64ec86218dcfa4ba4f96b8be60
hasnain <hasnain.tanvir@dengage.com>
2023-12-27 23:52:47 +0500

    libraries updated

--- ios/Classes/DengageCoordinator.swift
+++ ios/Classes/DengageCoordinator.swift
@@ -32,7 +32,8 @@ public class DengageCoordinator: NSObject {
         
         
         Dengage.promptForPushNotifications()
-        
+        Dengage.setHybridAppEnvironment()
+
         
     }
     
